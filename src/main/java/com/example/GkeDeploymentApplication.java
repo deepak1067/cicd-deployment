5794a4fb6f73325db8ba109bddc1ce4d2266e074
deepak1067 <deepak@knoldus.com>
2023-06-29 17:21:25 +0530

    first commit

---
+++ src/main/java/com/example/GkeDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkeDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GkeDeploymentApplication.class, args);
+	}
+
+}
